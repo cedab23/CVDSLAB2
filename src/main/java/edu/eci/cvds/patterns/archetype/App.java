be304e448d57f4ac05c9c23e9e0ec796282affb0
Cedab23 <cedab23@gmail.com>
2025-02-02 10:32:04 -0500

    feat: personalized greeting in App.java

--- src/main/java/edu/eci/cvds/patterns/archetype/App.java
+++ src/main/java/edu/eci/cvds/patterns/archetype/App.java
@@ -1,10 +1,16 @@
 package edu.eci.cvds.patterns.archetype;
 
+
 /**
  * Hello world!
  */
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        if (args.length > 0 ){
+        System.out.println("Hello "+args[0]+"!");
+        } else{
+            System.out.println("Hello world!");
+        }
     }
 }
